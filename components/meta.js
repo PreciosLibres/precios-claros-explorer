b30299768b39f644302ce30979de8c117b42ab5f
Diego Peralta <diego.peralta.dev@gmail.com>
2017-01-08 20:09:05 -0300

    add page title

--- components/meta.js
+++ components/meta.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 export default () => (
   <div>
     <Head>
+      <title>Precios Claros Explorer</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="stylesheet" href="https://unpkg.com/tachyons@4.6.1/css/tachyons.min.css"/>
     </Head>
