e18fddd87c96278e85d74062fcdbfb6a2b93987a
Diego Peralta <diego.peralta.dev@gmail.com>
2017-01-09 13:51:14 -0300

    add nprogres logic

--- components/meta.js
+++ components/meta.js
@@ -1,4 +1,10 @@
 import Head from 'next/head'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+
+Router.onRouteChangeStart = () => NProgress.start()
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done()
 
 export default () => (
   <div>
