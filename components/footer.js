2cbd1f352004c229e2ea1317e29a71d3ea3cebe9
Diego Peralta <diego.peralta.dev@gmail.com>
2017-01-14 17:37:48 -0300

    update footer legend

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,10 @@ import Link from 'next/prefetch'
 export default () => (
   <footer className="ph5 ph5-ns pv6 bt b--black-10 black-70">
     <a href="mailto:hola@precioslibres.org" className="link b f3 f2-ns dim black-70 lh-solid">hola@precioslibres.org</a>
-    <p className="f6 db b ttu lh-solid">©© 2017</p>
+    <p className="f6 db gray lh-solid">
+      ©© 2017 — Precios Claros Explorer es una herramienta desarrollada con
+      tecnologías <a href="#" className="link blue underline">Open Source</a> ❤️
+    .</p>
     <div className="mt5">
       <Link href="/">
         <a title="Ir al Inicio" className="f6 dib pr2 mid-gray dim">Inicio</a>
