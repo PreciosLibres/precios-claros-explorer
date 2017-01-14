9bd73e49fce7e212611ac61d40a1fe82c0df09f3
Diego Peralta <diego.peralta.dev@gmail.com>
2017-01-14 17:53:45 -0300

    add open souce page

--- components/footer.js
+++ components/footer.js
@@ -2,20 +2,32 @@ import Link from 'next/prefetch'
 
 export default () => (
   <footer className="ph5 ph5-ns pv6 bt b--black-10 black-70">
-    <a href="mailto:hola@precioslibres.org" className="link b f3 f2-ns dim black-70 lh-solid">hola@precioslibres.org</a>
+    <a
+      href="mailto:hola@precioslibres.org"
+      className="link b f3 f2-ns dim black-70 lh-solid">
+        hola@precioslibres.org
+    </a>
     <p className="f6 db gray lh-solid">
-      ©© 2017 — Precios Claros Explorer es una herramienta desarrollada con
-      tecnologías <a href="#" className="link blue underline">Open Source</a> ❤️
+      ©© 2017 — Precios Claros Explorer es una aplicación desarrollada con
+      herramientas
+      <Link href="open-source">
+         <a className="ml1 mr1 link blue underline">Open Source</a>
+      </Link>
+      ❤️
     .</p>
     <div className="mt5">
       <Link href="/">
         <a title="Ir al Inicio" className="f6 dib pr2 mid-gray dim">Inicio</a>
       </Link>
       <Link href="/sobre-precios-libres">
-        <a title="Sobre Precios Libres" className="f6 dib pr2 mid-gray dim">Sobre Precios Libres</a>
+        <a title="Sobre Precios Libres" className="f6 dib pr2 mid-gray dim">
+          Sobre Precios Libres
+        </a>
       </Link>
       <Link href="/terminos-de-uso">
-        <a title="Terminos de Uso" className="f6 dib ph2 mid-gray dim">Terminos de Uso</a>
+        <a title="Terminos de Uso" className="f6 dib ph2 mid-gray dim">
+          Terminos de Uso
+        </a>
       </Link>
       <Link href="https://github.com/PreciosLibres">
         <a title="Github" className="f6 dib ph2 mid-gray dim">Github</a>
---
+++ pages/open-source.js
@@ -0,0 +1,91 @@
+import React  from 'react'
+import Link   from 'next/prefetch'
+import Meta   from '../components/meta'
+import Footer from '../components/footer'
+
+export default class extends React.Component {
+  render () {
+    return (
+      <div>
+        <Meta />
+
+        <article className="pa3 pa5-ns">
+          <p>
+            <Link href="/">
+              <a className="link blue">← Volver a la página principal</a>
+            </Link>
+          </p>
+          <h1 className="f3 f2-m f1-l">Open Source</h1>
+          <p className="measure lh-copy">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+
+          <div className="cf">
+          <article className="fl mw5 mr4 bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+            <div className="tc">
+              <h1 className="f4">Precios Claros Dump</h1>
+              <hr className="mw3 bb bw1 b--black-10" />
+            </div>
+            <p className="lh-copy tc measure center f6 black-70">
+              Quite affectionate and outgoing.
+              She loves to get chin scratches and will
+              roll around on the floor waiting for you give her more of them.
+
+              <a
+                className="f6 center db br-pill bg-blue no-underline white ba grow pv2 ph3 tc mt4 mr3"
+                href="#"
+              >
+                Github
+              </a>
+            </p>
+          </article>
+          <article className="fl mw5 mr4 bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+            <div className="tc">
+              <h1 className="f4">Prcios Claros Explorer</h1>
+              <hr className="mw3 bb bw1 b--black-10" />
+            </div>
+            <p className="lh-copy tc measure center f6 black-70">
+              Quite affectionate and outgoing.
+              She loves to get chin scratches and will
+              roll around on the floor waiting for you give her more of them.
+
+              <a
+                className="f6 center db br-pill bg-blue no-underline white ba grow pv2 ph3 tc mt4 mr3"
+                href="#"
+              >
+                Github
+              </a>
+            </p>
+          </article>
+          <article className="fl mw5 mr4 bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+            <div className="tc">
+              <h1 className="f4">Ionic Inventory Mobile App</h1>
+              <hr className="mw3 bb bw1 b--black-10" />
+            </div>
+            <p className="lh-copy tc measure center f6 black-70">
+              Quite affectionate and outgoing.
+              She loves to get chin scratches and will
+              roll around on the floor waiting for you give her more of them.
+
+              <a
+                className="f6 center db br-pill bg-blue no-underline white ba grow pv2 ph3 tc mt4 mr3"
+                href="#"
+              >
+                Github
+              </a>
+            </p>
+          </article>
+          </div>
+
+        </article>
+
+
+
+        <Footer />
+      </div>
+    )
+  }
+}
