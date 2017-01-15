0c14ec12a93b85b95e8519c1ba6531910335ddc0
Diego Peralta <diego.peralta.dev@gmail.com>
2017-01-15 15:55:59 -0300

    add divider component to header/home-stats

--- components/header.js
+++ components/header.js
@@ -1,22 +1,26 @@
+import Divider from './divider'
 import SubscribeForm from './subscribe-form'
 
 export default () => (
-  <article className="pa3 pa5-ns">
-    <h1 className="f2 f1-l">
-      <i className="fa fa-grav mr3" aria-hidden="true"></i>
-      <span className="fw3">Precios</span> Libres
-    </h1>
-    <p className="measure lh-copy">
-      Es una <u>plataforma web</u> y una <u>aplicación para teléfonos inteligentes </u>
-       que facilita el <b>relevamiento de precios</b> y ayuda a <b>encontrar mejores
-      ofertas</b>.
-    </p>
-    <p className="measure lh-copy">
-      A diferencia de otras aplicaciones existentes, no limita la
-      información a precios de productos bajo acuerdo y enfatiza la
-      participación de los usuarios para mantener la información
-      actualizada, esto es, <b>inteligencia colectiva contra la inflación</b>.
-    </p>
-    <SubscribeForm />
-  </article>
+  <div>
+    <article className="pa3 pa5-ns">
+      <h1 className="f2 f1-l">
+        <i className="fa fa-grav mr3" aria-hidden="true"></i>
+        <span className="fw3">Precios</span> Libres
+      </h1>
+      <p className="measure lh-copy">
+        Es una <u>plataforma web</u> y una <u>aplicación para teléfonos inteligentes </u>
+         que facilita el <b>relevamiento de precios</b> y ayuda a <b>encontrar mejores
+        ofertas</b>.
+      </p>
+      <p className="measure lh-copy">
+        A diferencia de otras aplicaciones existentes, no limita la
+        información a precios de productos bajo acuerdo y enfatiza la
+        participación de los usuarios para mantener la información
+        actualizada, esto es, <b>inteligencia colectiva contra la inflación</b>.
+      </p>
+      <SubscribeForm />
+    </article>
+    <Divider />
+  </div>
 )
--- components/home-stats.js
+++ components/home-stats.js
@@ -1,34 +1,40 @@
+import Divider from './divider'
+
 export default () => (
-  <article className="pa3 pa5-ns" data-name="slab-stat-large">
-    <h3 className="f6 ttu tracked">
-      <i className="fa fa-area-chart mr1" aria-hidden="true"></i>
-      Precios Libres en números
-    </h3>
-    <div className="cf">
-      <dl className="db dib-l w-auto-l lh-title mr6-l">
-        <dd className="f6 fw4 ml0">Productos Únicos</dd>
-        <dd className="f2 f-subheadline-l fw6 ml0">1,024</dd>
-      </dl>
-      <dl className="db dib-l w-auto-l lh-title mr6-l">
-        <dd className="f6 fw4 ml0">Sucursales relevadas en todo el País</dd>
-        <dd className="f2 f-subheadline-l fw6 ml0">993</dd>
-      </dl>
-      <dl className="db dib-l w-auto-l lh-title mr6-l">
-        <dd className="f6 fw4 ml0">Total de Empresas</dd>
-        <dd className="f2 f-subheadline-l fw6 ml0">22</dd>
-      </dl>
-      <dl className="db dib-l w-auto-l lh-title mr6-l">
-        <dd className="f6 fw4 ml0">Total de Marcas</dd>
-        <dd className="f2 f-subheadline-l fw6 ml0">400</dd>
-      </dl>
-      <dl className="db dib-l w-auto-l lh-title mr6-l">
-        <dd className="f6 fw4 ml0">Marca con mayor cantidad de productos</dd>
-        <dd className="f2 f-subheadline-l fw6 ml0">Quilmes</dd>
-      </dl>
-      <dl className="db dib-l w-auto-l lh-title">
-        <dd className="f6 fw4 ml0">Promedio de costo de la canasta familiar</dd>
-        <dd className="f2 f-subheadline-l fw6 ml0">$1,200</dd>
-      </dl>
-    </div>
-  </article>
+  <div>
+    <article className="pa3 pa5-ns" data-name="slab-stat-large">
+      <h3 className="f6 ttu tracked">
+        <i className="fa fa-area-chart mr1" aria-hidden="true"></i>
+        Precios Libres en números
+      </h3>
+      <div className="cf">
+        <dl className="db dib-l w-auto-l lh-title mr6-l">
+          <dd className="f6 fw4 ml0">Productos Únicos</dd>
+          <dd className="f2 f-subheadline-l fw6 ml0">1,024</dd>
+        </dl>
+        <dl className="db dib-l w-auto-l lh-title mr6-l">
+          <dd className="f6 fw4 ml0">Sucursales relevadas en todo el País</dd>
+          <dd className="f2 f-subheadline-l fw6 ml0">993</dd>
+        </dl>
+        <dl className="db dib-l w-auto-l lh-title mr6-l">
+          <dd className="f6 fw4 ml0">Total de Empresas</dd>
+          <dd className="f2 f-subheadline-l fw6 ml0">22</dd>
+        </dl>
+        <dl className="db dib-l w-auto-l lh-title mr6-l">
+          <dd className="f6 fw4 ml0">Total de Marcas</dd>
+          <dd className="f2 f-subheadline-l fw6 ml0">400</dd>
+        </dl>
+        <dl className="db dib-l w-auto-l lh-title mr6-l">
+          <dd className="f6 fw4 ml0">Marca con mayor cantidad de productos</dd>
+          <dd className="f2 f-subheadline-l fw6 ml0">Quilmes</dd>
+        </dl>
+        <dl className="db dib-l w-auto-l lh-title">
+          <dd className="f6 fw4 ml0">Promedio de costo de la canasta familiar</dd>
+          <dd className="f2 f-subheadline-l fw6 ml0">$1,200</dd>
+        </dl>
+      </div>
+    </article>
+
+    <Divider />
+  </div>
 )
