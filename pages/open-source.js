3558225d641cb3ca4e745f2503e364ebf50213c2
Diego Peralta <diego.peralta.dev@gmail.com>
2017-01-16 15:41:19 -0300

    add twitter bot card

--- pages/open-source.js
+++ pages/open-source.js
@@ -15,7 +15,7 @@ export default class extends React.Component {
 
           <h1 className="f3 f2-m f1-l">Open Source</h1>
 
-          <p className="measure lh-copy">
+          <p className="measure lh-copy mb5">
             A lo largo del proceso de apertura de los datos de Precios Libres
             desarrollamos un set de herramientas y abrimos el código a la
             comunidad para que <b>programadores</b>, <b>economistas</b>, <b>periodistas</b> o cualquier
@@ -72,6 +72,24 @@ export default class extends React.Component {
               <p className="lh-copy tc measure center f6 black-70">
                 Aplicación de listas de compras para teléfonos móviles. Desarrollada en Javascript usando las librerias Ionic 2.0 y
                 Angular 2.0.
+                <a
+                  className="f6 center db br-pill bg-red no-underline white ba grow pv2 ph3 tc mt4 mr3"
+                  href="https://github.com/PreciosLibres/ionic-inventory-app"
+                >
+                  <i className="fa fa-github mr1" aria-hidden="true"></i>
+                  Github
+                </a>
+              </p>
+            </article>
+
+            <article className="fl mw5 mr4 bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+              <div className="tc">
+                <h1 className="f4">Twitter <br/> Bot</h1>
+                <hr className="mw3 bb bw1 b--black-10" />
+              </div>
+
+              <p className="lh-copy tc measure center f6 black-70">
+                Bot de alerta de aumenta indiscriminada de suba de precios vía twitter. Desarrollado en Python y Django.
                 <a
                   className="f6 center db br-pill bg-red no-underline white ba grow pv2 ph3 tc mt4 mr3"
                   href="https://github.com/PreciosLibres/ionic-inventory-app"
